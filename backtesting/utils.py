a6caad48902ce9b51768edfa021ca6c37bfefd63
LuisCastilloV98 <lui19982011@gmail.com>
2021-01-11 15:26:30 -0600

    Reparacion de error en fechas (se puede agregar la normalizacion por minutos)

--- backtesting/utils.py
+++ backtesting/utils.py
@@ -1,5 +1,6 @@
 from datetime import datetime
 import pandas as pd
+import pytz
 import yfinance as yf
 import backtrader as bt
 from pytrends.request import TrendReq
@@ -49,6 +50,38 @@ def fit_sentiment_days(data_frame_sentiment_news, data_frame_history):
     return df
 
 
+def fit_trends_days(data_trends, data_frame_history):
+    data_frame_history['datatime'] = data_frame_history.index
+    list_data = data_frame_history.values.tolist()
+    list_data = sorted(list_data, key=lambda a: a[-1])
+
+    list_trend = data_trends.values.tolist()
+    list_trend = sorted(list_trend, key=lambda a: a[0])
+
+    result = []
+    actual_pos_sentiment = 0
+    temp_val = list_trend[0][1]
+    for i in list_data:
+
+        if list_trend[actual_pos_sentiment][0] > i[-1]:
+            result.append(
+                [i[-1], temp_val])
+        else:
+            temp_val = list_trend[actual_pos_sentiment][1]
+            actual_pos_sentiment += 1
+
+            while list_trend[actual_pos_sentiment + 1][0] < i[-1]:
+                temp_val = list_trend[actual_pos_sentiment][1]
+                actual_pos_sentiment += 1
+            result.append(
+                [i[-1], temp_val])
+
+    df = pd.DataFrame(result, index=[i[0] for i in result],
+                      columns=['DateTime', 'Company'])
+
+    return df
+
+
 def add_data_to_cerebro(cerebro, ticker, internal, initial_date, final_date):
     # Add history data to cerebro
     stock_yf = yf.Ticker(ticker)
@@ -61,17 +94,22 @@ def add_data_to_cerebro(cerebro, ticker, internal, initial_date, final_date):
     cerebro.adddata(data_history_f)
 
     # Add data from google trends
-    py_trend = TrendReq()
-    #kw_list = ["amazon"]
+    py_trend = TrendReq(hl='en-US', tz=-360)
+    # kw_list = ["amazon"]
     kw_list = [ticker]
     py_trend.build_payload(kw_list, cat=7, timeframe='now 7-d',
                            geo='', gprop='news')
     data_trends = py_trend.interest_over_time()
+    index = data_trends.index
+    cet = pytz.timezone('America/Costa_Rica')
+    data_trends.index = index.tz_localize(pytz.utc).tz_convert(cet)
     data_trends['Date'] = data_trends.index
     del data_trends["isPartial"]
     var = data_trends[data_trends.columns[0]]
     del data_trends[data_trends.columns[0]]
     data_trends['Company'] = var
+
+    # data_trends = fit_trends_days(data_trends, data_frame_history)
 
     data_trends_f = bt.feeds.PandasData(
         dataname=data_trends,
@@ -103,6 +141,6 @@ def add_data_to_cerebro(cerebro, ticker, internal, initial_date, final_date):
 
     cerebro.adddata(data_news_f)
 
-    #Return cerebro
+    # Return cerebro
 
     return cerebro
